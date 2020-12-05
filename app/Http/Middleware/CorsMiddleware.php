6fa4ef8a0900263341c819d6f16498df29737dca
danitw <twz3xc21@gmail.com>
2020-12-05 17:22:51 -0300

    add headers

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,9 +15,6 @@ class CorsMiddleware
   public function handle($request, Closure $next)
   {
     $headers = [
-      'Access-Control-Allow-Origin' => '*',
-      'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
-      'Access-Control-Allow-Credentials' => 'true',
       'Access-Control-Max-Age' => '86400',
       'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'
     ];
